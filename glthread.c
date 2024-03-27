deb14b2bb56b01d00004f7b79a2f856b1ce89662
Vedansh Bansal <careervedansh@gmail.com>
2024-03-27 16:04:44 +0700

    Updated some parts in glued linked API, glthread.c basically

--- glthread.c
+++ glthread.c
@@ -21,4 +21,51 @@ void glthread_add_next(glthread_t *curr_glthread, glthread_t *new_glthread)
 	}
 
 	glthread_t *temp = curr_glthread->right;
+	curr_glthread->right = new_glthread;
+	new_glthread->left = curr_glthread;
+	new_glthread->right = temp;
+	temp->left = new_glthread;
+}
+
+void glthread_add_before(glthread_t *curr_glthread, glthread_t *new_glthread)
+{
+  if(!curr_glthread->left)
+  {
+    new_glthread->left = NULL;
+    new_glthread->right = curr_glthread;
+    curr_glthread->left = new_glthread;
+    return;
+  }
+
+  glthread_t *temp = curr_glthread->left;
+  new_glthread->right = curr_glthread;
+  curr_glthread->left = new_glthread;
+  temp->right = new_glthread;
+  new_glthread->left = temp;
+}
+
+void remove_glthread(glthread_t *curr_glthread)
+{
+  if(!curr_glthread->left)
+  {
+    if(curr_glthread->right)
+    {
+      curr_glthread->right->left = NULL;
+      curr_glthread->right = NULL;
+      return;
+    }
+    return;
+  }
+
+  if(!curr_glthread->right)
+  {
+    curr_glthread->left->right = NULL;
+    curr_glthread->left = NULL;
+    return;
+  }
+
+  curr_glthread->left->right = curr_glthread->right;
+  curr_glthread->right->left = curr_glthread->left;
+  curr_glthread->right = NULL;
+  curr_glthread->left = NULL;
 }
