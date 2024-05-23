c847a5b22da4b15add0fc6e391fb77aab5a4e7dc
Vedansh Bansal <careervedansh@gmail.com>
2024-05-23 16:36:32 +0700

    setting network properties

--- topologies.c
+++ topologies.c
@@ -6,7 +6,7 @@ build_first_topo(){
 #if 0
 
                           +----------+
-                      0/4 |          |0/0
+                      0/5 |          |0/0
          +----------------+   R0_re  +---------------------------+
          |                |          |                           |
          |                +----------+                           |
@@ -14,7 +14,7 @@ build_first_topo(){
          |                                                       |
          |                                                       |
          |                                                       |
-         |0/5                                                    |0/1
+         |0/4                                                    |0/1
      +---+---+                                              +----+-----+
      |       |0/3                                        0/2|          |
      | R2_re +----------------------------------------------+    R1_re |
@@ -31,7 +31,19 @@ build_first_topo(){
 
     insert_link_between_two_nodes(R0_re, R1_re, "eth0/0", "eth0/1", 1);
     insert_link_between_two_nodes(R1_re, R2_re, "eth0/2", "eth0/3", 1);
-    insert_link_between_two_nodes(R0_re, R2_re, "eth0/4", "eth0/5", 1);
+    insert_link_between_two_nodes(R0_re, R2_re, "eth0/5", "eth0/4", 1);
+
+    node_set_loopback_address(R0_re, "122.1.1.0");
+    node_set_intf_ip_address(R0_re, "eth0/0", "20.1.1.1", 24);
+    node_set_intf_ip_address(R0_re, "eth0/5", "40.1.1.1", 24);
+
+    node_set_loopback_address(R1_re, "122.1.1.1");
+    node_set_intf_ip_address(R1_re, "eth0/1", "20.1.1.2", 24);
+    node_set_intf_ip_address(R1_re, "eth0/2", "30.1.1.1", 24);
+
+    node_set_loopback_address(R2_re, "122.1.1.2");
+    node_set_intf_ip_address(R2_re, "eth0/3", "30.1.1.2", 24);
+    node_set_intf_ip_address(R2_re, "eth0/4", "40.1.1.2", 24);
 
     return topo;
 }
